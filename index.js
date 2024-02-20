f0f0cd0a6b52bc114b32521ff5a3a45310d37a1e
Bahman World <bahman.world@gmail.com>
2024-02-21 01:14:59 +0330

    first commit

--- index.js
+++ index.js
@@ -1,43 +1,13 @@
-"use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
+const express = require("express");
+
+const app = express();
+
+app.get("/api", (req, res) => {
+  res.json({ message: "hello world!" });
 });
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-const trpcExpress = __importStar(require("@trpc/server/adapters/express"));
-const trpc_1 = require("./trpc");
-const app = (0, express_1.default)();
-app.get("/api/upload", (req, res) => {
-    res.json({ result: { data: "all files uploaded", success: true } });
-});
-// app.use("/api", trpcExpress.createExpressMiddleware({
-//     router: trpc_1.appRouter,
-//     createContext: trpc_1.createContext,
-// }));
+
 app.listen(3000, () => {
-    console.log("listening on port 3000...");
+  console.log("listening on port 3000...");
 });
+
 module.exports = app;
