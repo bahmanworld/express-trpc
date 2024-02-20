4453bfbe9a5558b58f8e2073e945c5567dcb1420
Bahman World <bahman.world@gmail.com>
2024-02-21 00:47:00 +0330

    first commit

--- index.ts
+++ index.ts
@@ -19,6 +19,8 @@ app.use(
 
 
 
-app.listen(3000, () => {
-  console.log("listening on port 3000...");
-});
+// app.listen(3000, () => {
+//   console.log("listening on port 3000...");
+// });
+
+export default app
